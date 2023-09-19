f936520d584ad313c562c6373cb9cdd2accdeed4
Tobi1001 <thebrimstone1@gmail.com>
2023-09-19 17:52:15 -0500

    Agregados comentarios al codigo, no se que le cambie a la historia

--- pilas.java
+++ pilas.java
@@ -9,38 +9,38 @@ public class pilas{
 
 class Sarray<T>{
 
-    private int top;
+    private int top;//indica la posicion en donde se insertará un elemento
     private T[] sarray;
 
 
     Sarray(int i){
-        top = 0;
-        sarray = (T[]) new Object[i];
+        top = 0;//la posicion inicial de los elementos será 0
+        sarray = (T[]) new Object[i];//lista con tamaño en el argumento del constructor
     }
 
     Sarray(){
-        this(4);
+        this(4);//el constructor sin argumentos hará a la lista de tamaño 4
     }
 
     public void push(T element){
         if(full())
             throw new RuntimeException("La pila ya está llena");
-        sarray[top] = element;
-        top++;
+        sarray[top] = element;//se llena la posicion top
+        top++;  //se incrementa un espacio
     }
 
     public T pop(){
         if(empty())
             throw new RuntimeException("La pila ya está vacía");
-        top--;
-        return sarray[top];
+        top--;              //se reduce el top
+        return sarray[top]; //retorna el elemento que quitamos, no es necesario hacerlo null ya que al hacer un push por ejemplo, este será reemplazado (-operaciones, + eficiencia)
     }
 
     public boolean full(){
-        return top >= sarray.length;
+        return top >= sarray.length; //el top solo puede ser igual al length, el ">" es solo una medida preventiva
     }
 
     public boolean empty(){
-        return top <= 0;
+        return top <= 0;//el top al ser 0 es vacio ya que es la posicion inicial del array
     }
 }
